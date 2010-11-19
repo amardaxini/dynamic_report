525a2c9bebf4c5fa3d9ccca9deaf6e9bbff5f33e
amardaxini <amardaxini@gmail.com>
2010-11-19 12:29:44 +0530

    added flying saucer config

---
+++ config/initializers/flying_saucer.rb
@@ -0,0 +1,6 @@
+ActsAsFlyingSaucer::Config.options = {
+  :java_bin => "java",          # java binary
+  :classpath_separator => ':',  # classpath separator. unixes system use ':' and windows ';'
+  :tmp_path => "/tmp",          # path where temporary files will be stored
+  :nailgun =>false
+}
